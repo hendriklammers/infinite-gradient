68c435ed7dce9c669642f3b31930b48ff1160831
Hendrik Lammers <lammers@gmail.com>
2017-03-03 23:06:10 +0100

    Add Most.js stream library

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import './styles.css'
+import {fromEvent} from 'most'
 
-let amountY = 0
+// const styles = document.documentElement.style
+// const colors = ['red', 'blue', 'yellow', 'green', 'purple', 'black']
+// styles.setProperty('--color-top', 'blue')
 
-document.addEventListener('wheel', event => {
-  event.preventDefault()
-  amountY += event.deltaY
-  console.log('amountY', amountY)
-})
+// let amountY = 0
+//
+// const randomColor = () => '#' + ((1<<24) * Math.random() | 0).toString(16)
+//
+// const handleScroll = event => {
+//   event.preventDefault()
+//   amountY += event.deltaY
+// }
+//
+// document.addEventListener('wheel', handleScroll)
+
+const add = (a, b) => a + b
+const scroll = fromEvent('wheel', document)
+
+scroll.map(event => event.deltaY)
+  .scan(add, 0)
+  .observe(val => console.log(val))

66e6110b8adc8f5c6d727d8799558dc71761e3cd
Hendrik Lammers <lammers@gmail.com>
2017-03-11 20:38:28 +0100

    Tween two colors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import './styles.css'
 import {fromEvent} from 'most'
+import randomColor from 'randomcolor'
 
-// const styles = document.documentElement.style
 // const colors = ['red', 'blue', 'yellow', 'green', 'purple', 'black']
-// styles.setProperty('--color-top', 'blue')
 
 // let amountY = 0
 //
@@ -16,9 +15,24 @@ import {fromEvent} from 'most'
 //
 // document.addEventListener('wheel', handleScroll)
 
+const styles = document.documentElement.style
 const add = (a, b) => a + b
-const scroll = fromEvent('wheel', document)
+const lerp = (n, min, max) => (max - min) * n + min
+const normalize = (n, min, max) => (n - min) / (max - min)
+const rgbToArr = str => str.replace(/[^\d,]/g, '').split(',').map(n => parseInt(n))
+const colors = randomColor({format: 'rgb', count: 3}).map(rgbToArr)
+const arrToRgb = arr => `rgb(${arr[0]},${arr[1]},${arr[2]})`
+const scroll$ = fromEvent('wheel', document)
 
-scroll.map(event => event.deltaY)
+styles.setProperty('--color-top', arrToRgb(colors[1]))
+
+scroll$.map(event => event.deltaY)
   .scan(add, 0)
-  .observe(val => console.log(val))
+  .observe(val => {
+    const color = colors[1].map((c, i) => {
+      return Math.round(lerp(normalize(val, 0, 1000), c, colors[2][i]))
+    })
+    console.log('color', arrToRgb(color))
+    styles.setProperty('--color-top', arrToRgb(color))
+
+  })

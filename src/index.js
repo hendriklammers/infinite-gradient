16f33a4347be555a0210078318168706c6a3a354
Hendrik Lammers <lammers@gmail.com>
2017-03-11 22:32:51 +0100

    Add clamp

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import randomColor from 'randomcolor'
 
 const styles = document.documentElement.style
 const add = (a, b) => a + b
+const clamp = (val, min, max) => Math.min(Math.max(val, min), max)
 const lerp = (n, min, max) => (max - min) * n + min
 const normalize = (n, min, max) => (n - min) / (max - min)
 const rgbToArr = str => str.replace(/[^\d,]/g, '').split(',').map(n => parseInt(n))
@@ -17,7 +18,7 @@ scroll$.map(event => event.deltaY)
   .scan(add, 0)
   .observe(val => {
     const color = colors[1].map((c, i) => {
-      return Math.round(lerp(normalize(val, 0, 1000), c, colors[2][i]))
+      return clamp(Math.round(lerp(normalize(val, 0, 1000), c, colors[2][i])), 0, 255)
     })
     console.log('color', arrToRgb(color))
     styles.setProperty('--color-top', arrToRgb(color))

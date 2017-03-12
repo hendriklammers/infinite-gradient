ee97be658e860cc681ccade40fd33367d2ee2eaf
Hendrik Lammers <lammers@gmail.com>
2017-03-12 10:12:06 +0100

    Add tweenColor function

--- src/index.js
+++ src/index.js
@@ -7,20 +7,22 @@ const add = (a, b) => a + b
 const clamp = (val, min, max) => Math.min(Math.max(val, min), max)
 const lerp = (n, min, max) => (max - min) * n + min
 const normalize = (n, min, max) => (n - min) / (max - min)
-const rgbToArr = str => str.replace(/[^\d,]/g, '').split(',').map(n => parseInt(n))
+const rgbToArr = str =>
+  str.replace(/[^\d,]/g, '').split(',').map(n => parseInt(n))
 const colors = randomColor({format: 'rgb', count: 3}).map(rgbToArr)
 const arrToRgb = arr => `rgb(${arr[0]},${arr[1]},${arr[2]})`
 const scroll$ = fromEvent('wheel', document)
+const tweenColor = (c1, c2, val) => c1.map((c, i) =>
+  clamp(Math.round(lerp(normalize(val, 0, 1000), c, c2[i])), 0, 255))
 
 styles.setProperty('--color-top', arrToRgb(colors[1]))
 
 scroll$.map(event => event.deltaY)
   .scan(add, 0)
   .observe(val => {
-    const color = colors[1].map((c, i) => {
-      return clamp(Math.round(lerp(normalize(val, 0, 1000), c, colors[2][i])), 0, 255)
-    })
-    console.log('color', arrToRgb(color))
-    styles.setProperty('--color-top', arrToRgb(color))
+    val = val % 1000
+    const color = arrToRgb(tweenColor(colors[1], colors[2], val))
+    console.log('color', color)
+    styles.setProperty('--color-top', color)
 
   })

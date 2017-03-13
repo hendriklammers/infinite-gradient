921d1d199f4e820859afca83e4c91a2d25b0fd01
Hendrik Lammers <lammers@gmail.com>
2017-03-13 21:46:25 +0100

    More function composition

--- src/index.js
+++ src/index.js
@@ -4,14 +4,23 @@ import {compose, curry, map} from 'ramda'
 import randomColor from 'randomcolor'
 
 const styles = document.documentElement.style
-const tweenDuration = 2000
+const TWEEN_DISTANCE = 2000
 
+// Default curried clamp function
 const clamp = curry((min, max, n) => Math.min(Math.max(n, min), max))
 const clampRgb = clamp(0, 255)
 
-const lerp = (n, min, max) => (max - min) * n + min
-const normalize = (n, min, max) => (n - min) / (max - min)
+// Default curried normalize function
+const normalize = curry((min, max, n) => (n - min) / (max - min))
+const normalizeTween = normalize(0, TWEEN_DISTANCE)
+
+// Default curried linear interpolation function
+const lerp = curry((min, max, n) => (max - min) * n + min)
+
+// RGB string to array: rgb(255,255,255) -> [255,255,255]
 const rgbToArr = str => str.replace(/[^\d,]/g, '').split(',')
+
+// RGB array to string: [255,255,255] -> rgb(255,255,255)
 const arrToRgb = arr => `rgb(${arr[0]},${arr[1]},${arr[2]})`
 
 // Uses randomColor library to create a new rgb color array
@@ -22,8 +31,10 @@ const newColor = compose(
 )
 
 // Calculates interpolated color from two rgb arrays
-const tweenColor = (c1, c2, val) => c1.map((c, i) =>
-  clampRgb(Math.round(lerp(normalize(val, 0, tweenDuration), c, c2[i]))))
+const tweenColors = (c1, c2, val) => c1.map((c, i) =>
+  compose(clampRgb, Math.round, lerp(c, c2[i]), normalizeTween)(val)
+)
+  // clampRgb(Math.round(lerp(c, c2[i], normalize(0, TWEEN_DISTANCE, val)))))
 
 // Stream of mousewheel scroll events
 const scroll$ = fromEvent('wheel', document)
@@ -33,8 +44,8 @@ const scroll$ = fromEvent('wheel', document)
 scroll$
   .map(event => event.deltaY)
   .scan((acc, val) => {
-    const prev = Math.floor(acc.total / tweenDuration)
-    const current = Math.floor((acc.total + val) / tweenDuration)
+    const prev = Math.floor(acc.total / TWEEN_DISTANCE)
+    const current = Math.floor((acc.total + val) / TWEEN_DISTANCE)
     if (current > prev) {
       acc.color1 = acc.color2
       acc.color2 = newColor()
@@ -51,10 +62,10 @@ scroll$
   })
   .observe(data => {
     // console.log('data', data)
-    // val = val % tweenDuration
-    const colorTop = arrToRgb(tweenColor(data.color1, data.color2, data.total % tweenDuration))
+    // val = val % TWEEN_DISTANCE
+    const colorTop = arrToRgb(tweenColors(data.color1, data.color2, data.total % TWEEN_DISTANCE))
     styles.setProperty('--color-top', colorTop)
 
-    // const colorBottom = arrToRgb(tweenColor(colors[2], colors[3], val))
+    // const colorBottom = arrToRgb(tweenColors(colors[2], colors[3], val))
     // styles.setProperty('--color-bottom', colorBottom)
   })
